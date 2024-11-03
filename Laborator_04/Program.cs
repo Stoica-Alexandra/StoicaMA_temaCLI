0695fbdec836a322164b95ac3add3f3e7c89255c
Maria Alexandra Stoica <alexandra.stoica@student.usv.ro>
2024-11-03 11:23:50 +0200

    Laborator #04
    Utilizatorul generează obiecte 3D aleatoare prin clicuri cu mouse-ul, care cad sub influența gravitației până la atingerea solului. Aplicația permite navigarea în scenă, schimbarea culorii de fundal și activarea sau dezactivarea vizibilității axelor și a grilei, precum și resetarea scenei la valorile implicite.

---
+++ Laborator_04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Stoica Maria Alexandra 3131B
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator_04
+{
+    /// <summary>
+    /// The main entry point for the application. This class initializes and runs the 3D rendering window.
+    /// </summary>
+    internal class Program
+    {
+
+        /// <summary>
+        /// The main method, which serves as the entry point for the application.
+        /// Initializes a new instance of <see cref="Window3D"/> and starts the rendering loop.
+        /// </summary>
+        /// <param name="args">Command-line arguments (not used in this application).</param>
+        static void Main(string[] args)
+        {
+            using (Window3D example = new Window3D())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
